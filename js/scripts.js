53d9a431463caf156573d7c661c903eced2027bd
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-01 11:55:01 -0700

    created basic html formatting, and finished javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 // business logic
-var pigWord = function(word) {
+var pingPong = function(number, output) {
 
+  for (index = 1; index <= number; index++) {
+    var temp = index;
+    if (temp % 5 === 0 && temp % 3 === 0) {
+      temp = "pingpong";
+    }
+    else if (temp % 5 === 0) {
+      temp = "pong";
+    }
+    else if (temp % 3 === 0) {
+      temp = "ping";
+    }
+    $(output).append("<li>" + temp + "</li>");
+  }
 };
 
 
@@ -9,6 +22,14 @@ var pigWord = function(word) {
 $(document).ready(function(){
   $("#input").submit(function(event) {
     event.preventDefault();
-
+    //get value from form
+    var number = parseInt($("#number").val());
+    //check for number input
+    if (isNaN(number)) {
+      alert("Please enter a number!");
+    } else {
+      //call pingPong function
+      pingPong(number, "#number-list");
+    }
   });
 });
