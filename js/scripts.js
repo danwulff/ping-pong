f3d8cc07ee9a1a66f41f23917be71797c07cdfb0
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-01 12:32:06 -0700

    few minor edits, fixed bug in javascript whereby previous list was not being cleared between submissions

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 // business logic
+//takes a number and html id (for an <ul>) and prints out list elements following the ping pong pattern
 var pingPong = function(number, output) {
-
   for (index = 1; index <= number; index++) {
+    //temp variable that will be changed to string if criteria is met
     var temp = index;
+    //if divisble by 5 & 3, print pingpong
     if (temp % 5 === 0 && temp % 3 === 0) {
       temp = "pingpong";
     }
+    //if divisble by 5 print pong
     else if (temp % 5 === 0) {
       temp = "pong";
     }
+    //if divisble by 3 print ping
     else if (temp % 3 === 0) {
       temp = "ping";
     }
+    //append list item
     $(output).append("<li>" + temp + "</li>");
   }
 };
-
 
 
 // user interface logic
@@ -28,6 +32,8 @@ $(document).ready(function(){
     if (isNaN(number)) {
       alert("Please enter a number!");
     } else {
+      //clear previous list
+      $("#number-list").html("");
       //call pingPong function
       pingPong(number, "#number-list");
     }
