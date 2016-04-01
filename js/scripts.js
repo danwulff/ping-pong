16cae853260cddcd9be7abac0817e497e96e49f1
Daniel Wulff <danielwulff.ee@gmail.com>
2016-04-01 11:05:19 -0700

    created intitial skeleton and link files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// business logic
+var pigWord = function(word) {
+
+};
+
+
+
+// user interface logic
+$(document).ready(function(){
+  $("#input").submit(function(event) {
+    event.preventDefault();
+
+  });
+});
